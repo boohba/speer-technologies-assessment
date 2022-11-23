013fc33b0cbe5778837bef06ef89fb5503fee7f2
Dmytro Prokhorov <d.prokhorov.245@gmail.com>
2022-11-23 00:23:50 -0500

    Fix tests

--- tests/endpoint.rs
+++ tests/endpoint.rs
@@ -1,20 +1,24 @@
-#[tokio::test]
+use once_cell::sync::{Lazy, OnceCell};
+
+#[tokio::test(flavor = "current_thread")]
 async fn main() {
     test_404().await;
     test_users().await;
     test_sessions().await;
 }
 
-static CLIENT: once_cell::sync::Lazy<reqwest::Client> = once_cell::sync::Lazy::new(|| {
+static CLIENT: Lazy<reqwest::Client> = Lazy::new(|| {
     reqwest::Client::builder()
         .danger_accept_invalid_certs(true)
         .build()
         .unwrap()
 });
 
+static TOKEN: OnceCell<String> = OnceCell::new();
+
 // might not be the most readable way to write tests but it works really well in a time-constrained environment
 macro_rules! test {
-    ($method:ident, $url:ident, $status:ident, $response:literal, $body:literal $(, $key:ident: $value:literal)*) => {
+    ($method:ident, $url:expr, $status:ident, $response:literal, $body:literal $(, $key:ident: $value:expr)*) => {
         let response = CLIENT.$method($url)
             $(.header(reqwest::header::$key, $value))*
             .body($body)
@@ -44,6 +48,15 @@ async fn test_users() {
     const URL: &str = "https://localhost:8443/users";
 
     test!(get, URL, METHOD_NOT_ALLOWED, r#"{"error":true,"message":"Method Not Allowed"}"#, "");
+
+    test_users_post().await;
+}
+
+async fn test_users_post() {
+    println!("test_users_post");
+
+    const URL: &str = "https://localhost:8443/users";
+
     test!(post, URL, BAD_REQUEST, r#"{"error":true,"message":"Content-Type is not set"}"#, "");
     test!(post, URL, UNSUPPORTED_MEDIA_TYPE, r#"{"error":true,"message":"Unsupported Media Type"}"#, "", CONTENT_TYPE: "application/x-www-form-urlencoded");
     test!(post, URL, BAD_REQUEST, r#"{"error":true,"message":"Invalid request payload"}"#, "aaa", CONTENT_TYPE: "application/json");
@@ -59,6 +72,15 @@ async fn test_sessions() {
     const URL: &str = "https://localhost:8443/sessions";
 
     test!(get, URL, METHOD_NOT_ALLOWED, r#"{"error":true,"message":"Method Not Allowed"}"#, "");
+
+    test_sessions_post().await;
+}
+
+async fn test_sessions_post() {
+    println!("test_sessions_post");
+
+    const URL: &str = "https://localhost:8443/sessions";
+
     test!(post, URL, BAD_REQUEST, r#"{"error":true,"message":"Content-Type is not set"}"#, "");
     test!(post, URL, UNSUPPORTED_MEDIA_TYPE, r#"{"error":true,"message":"Unsupported Media Type"}"#, "", CONTENT_TYPE: "application/x-www-form-urlencoded");
     test!(post, URL, BAD_REQUEST, r#"{"error":true,"message":"Invalid request payload"}"#, "aaa", CONTENT_TYPE: "application/json");
@@ -66,5 +88,26 @@ async fn test_sessions() {
     test!(post, URL, BAD_REQUEST, r#"{"error":true,"message":"Invalid request payload"}"#, r#"{"username":"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa","password":"aaa"}"#, CONTENT_TYPE: "application/json");
     test!(post, URL, NOT_FOUND, r#"{"error":true,"message":"Not Found"}"#, r#"{"username":"foo","password":"bar"}"#, CONTENT_TYPE: "application/json");
     test!(post, URL, UNAUTHORIZED, r#"{"error":true,"message":"Unauthorized"}"#, r#"{"username":"hello","password":"bar"}"#, CONTENT_TYPE: "application/json");
-    test!(post, URL, CREATED, r#"{"error":false,"result":"SXEtehSxJQO2XIxLl/gnGLItphjT3t62pVUYVMAmU4RJcS16FLElA7ZcjEuX+CcYsi2mGNPe3ralVRhUwCZThAAAAAAAAAAA"}"#, r#"{"username":"hello","password":"world"}"#, CONTENT_TYPE: "application/json");
+
+    let response = CLIENT
+        .post(URL)
+        .body(r#"{"username":"hello","password":"world"}"#)
+        .header(http::header::CONTENT_TYPE, "application/json")
+        .send()
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), reqwest::StatusCode::CREATED);
+
+    #[derive(serde::Deserialize)]
+    struct Response {
+        error: bool,
+        result: String,
+    }
+
+    let response = serde_json::from_slice::<Response>(&response.bytes().await.unwrap()).unwrap();
+
+    assert_eq!(response.error, false);
+
+    TOKEN.set(response.result).unwrap();
 }
